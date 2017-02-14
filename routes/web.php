d8c9337f1ea17998aa8c506c5589eaa30308b981
vagrant <vagrant@homestead>
2017-02-14 09:00:11 +0000

    Remove redundant routes

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,3 @@ Route::get('redis', function () {
     // this checks for the event
     return view('redis');
 });
-
-Route::get('fire', function () {
-    // this fires the event
-    event(new App\Events\TestEvent(10));
-    return "event fired";
-});
-Route::get('redis', function () {
-    // this checks for the event
-    return view('redis');
-});
-
